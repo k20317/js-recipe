725bf4abb6c70365c6a091b3b4a3fd596ee954e1
k20317 <k20726tm@gmail.com>
2021-08-05 10:22:17 +0900

    ATM

--- 02_レシピ/02_Vue ATM/main.js
+++ 02_レシピ/02_Vue ATM/main.js
@@ -1,39 +1,53 @@
 new Vue({
   el: "#v-model",
   data: {
-    nyuryokugaku: "",
-    zandaka: "",
+    nyuuryokugaku: 0,
+    zandaka: 0,
+    logs: [
+      {
+        date: new Date(),
+        type: "入金",
+        move: 1000,
+        money: 1000,
+      },
+      {
+        date: new Date(),
+        type: "出金",
+        move: 1000,
+        money: 1000,
+      },
+    ],
   },
+
   methods: {
-    nyukin: function() {
+    nyuukin: function() {
       // inputで入力される値はString型なので、Number() でNumber型に変換しないといけない。
-      this.zandaka += Number(this.nyuryokugaku)
+      this.zandaka += Number(this.nyuuryokugaku)
     },
     shuukin: function() {
       // inputで入力される値はString型なので、Number() でNumber型に変換しないといけない。
-      this.zandaka -= Number(this.nyuryokugaku)
+      this.zandaka -= Number(this.nyuuryokugaku)
     },
   },
 
-  logs: [
-    {
-      date: new Date("2021-07-23T12:00:00"),
-      type: "入金",
-      money: 1000,
+  methods: {
+    nyuukin: function() {
+      this.zandaka += Number(this.nyuuryokugaku) //Number型に変換する
+      let nowTime = new Date() //現在時刻
+      console.log(nowTime)
     },
-    {
-      date: new Date("2021-07-23T13:00:00"),
-      type: "出金",
-      money: 1000,
+
+    shukkin: function() {
+      this.zandaka -= Number(this.nyuuryokugaku)
     },
-  ],
+  },
 })
 
-// new Date() で現在時刻を取得できる
-const now = new Date()
+// // new Date() で現在時刻を取得できる
+// const now = new Date();
 
-// console.logやVueで表示させると、とりあえず良い感じで表示される
-console.log(now) // Tue Jul 27 2021 21:34:33 GMT+0900 (日本標準時)
+// // console.logやVueで表示させると、とりあえず良い感じで表示される
+// console.log(now); // Tue Jul 27 2021 21:34:33 GMT+0900 (日本標準時)
 
-// 文字列で特定の日付時刻を指定することもできる
-const birthday = new Date("1995-12-17T03:24:00")
+// // 文字列で特定の日付時刻を指定することもできる
+// const birthday = new Date("1995-12-17T03:24:00");
